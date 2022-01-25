96e7d68a31ea2ad32da850298203daf2e3b7df64
John Livee Oroncillo <johnliveeoroncillo@gmail.com>
2022-01-25 21:43:53 +0800

    UI fixes

--- components/template/header.vue
+++ components/template/header.vue
@@ -1,47 +1,48 @@
 <template>
-    <header class="w-full flex flex-row py-2 border-b items-center justify-between">
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/TikTok_logo.svg/2560px-TikTok_logo.svg.png" class="h-[30px]" />
+    <header class="fixed z-10 top-0 left-1/2 -translate-x-1/2 bg-white h-[63px] w-full border-b">
+        <div id="header-main" class="flex flex-row py-2 items-center justify-around mx-auto">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/TikTok_logo.svg/2560px-TikTok_logo.svg.png" class="h-[30px]" />
 
-        <div class="relative hidden md:block">
-            <input type="text" class="rounded-full focus:border focus:border-gray-300 border-gray-200 bg-gray-200 px-5 gap-4 h-[46px] md:w-full lg:w-[350px] pr-20 text-gray-600" placeholder="Search accounts and videos" />
-            <div class="border-l border-gray-300 absolute top-1/2 -translate-y-1/2 right-5 pl-3">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-            </div>
-        </div>
-
-        <div class="flex flex-row">
-            <button class="font-bold px-4 py-2">Upload</button>
-            <button class="font-bold px-8 py-2 rounded bg-primary text-white whitespace-nowrap">Log In</button>
-            <div class="relative">
-                <button class="font-bold px-4 py-2 hidden sm:block" @click="show = !show">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            <div class="relative hidden md:block">
+                <input type="text" class="rounded-full border focus:border-gray-300 border-gray-100 bg-gray-100 px-5 gap-4 h-[46px] md:w-full lg:w-[350px] pr-20 text-gray-600" placeholder="Search accounts and videos" />
+                <div class="border-l border-gray-300 absolute top-1/2 -translate-y-1/2 right-5 pl-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
-                </button>
-                <div class="absolute flex flex-col gap-4 right-0 top-12 p-5 z-10 bg-white rounded shadow" style="width: max-content" v-if="show">
-                    <nuxt-link to="/" class="flex flex-row gap-4 font-bold items-center text-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
-                        </svg>
-                        English
-                    </nuxt-link>
-                    <nuxt-link to="/" class="flex flex-row gap-4 font-bold items-center text-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                        Feedback and help
-                    </nuxt-link>
-                    <nuxt-link to="/" class="flex flex-row gap-4 font-bold items-center text-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                        Keyboard Shortcuts
-                    </nuxt-link>
                 </div>
             </div>
 
+            <div class="flex flex-row">
+                <button class="font-bold px-4 py-2">Upload</button>
+                <button class="font-bold px-8 py-2 rounded bg-primary text-white whitespace-nowrap">Log In</button>
+                <div class="relative">
+                    <button class="font-bold px-4 py-2 hidden sm:block" @click="show = !show">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                        </svg>
+                    </button>
+                    <div class="absolute flex flex-col right-0 top-12 py-2 z-10 bg-white rounded shadow" style="width: max-content" v-if="show">
+                        <nuxt-link to="/" class="flex flex-row gap-4 font-bold items-center hover:bg-gray-50 py-3 px-5 text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
+                            </svg>
+                            English
+                        </nuxt-link>
+                        <nuxt-link to="/" class="flex flex-row gap-4 font-bold items-center hover:bg-gray-50 py-3 px-5 text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            Feedback and help
+                        </nuxt-link>
+                        <nuxt-link to="/" class="flex flex-row gap-4 font-bold items-center hover:bg-gray-50 py-3 px-5 text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            Keyboard Shortcuts
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
     </header>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,144 @@
 <template>
-    <div class="w-screen h-screen overflow-hidden">
-        <div class="flex flex-col w-full h-full">
-            <template-header class="px-8 xl:px-40 2xl:px-96" />
-            <template-nav class="flex-1 xl:px-40 2xl:px-96" />
+    <div class="overflow-hidden flex flex-col w-full h-screen max-w-[1150px] mx-auto">
+        <template-header />
+
+        <div class="main flex-1 flex flex-row justify-center pt-[63px] w-full">
+            <template-sidenav />
+            <perfect-scrollbar class="relative w-full lg:w-[692px]">
+                
+                <div class="flex flex-col p-8 gap-8">
+
+                    <div class="inline-flex flex-col gap-4 border-b pb-8">
+                        <div class="flex flex-row items-start gap-4">
+                            <img loading="lazy" src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/a50a0eff18403fdbf1d0b25bc03976e5.jpeg?x-expires=1643191200&amp;x-signature=NtwHCdDuRENkzcbnoqaCbNdjC0I%3D" class="h-16 w-16 rounded-full">
+                            <span class="flex-1 pt-2">
+                                <span class="font-black">
+                                    USERNAME
+                                    <span class="font-thin text-xs opacity-60">JOHN</span>
+                                </span>
+
+                                <p class="font-thin tracking-wide opacity-80">
+                                    Nulla adipisicing laborum deserunt est ut eiusmod cillum quis qui.
+                                </p>
+                            </span>
+
+                            <button class="border-primary text-primary border rounded px-5 font-semibold tracking-wider">Follow</button>
+                        </div>
+
+                        <div class="h-[582px] flex flex-row pl-20 gap-4">
+                            <div class="rounded">
+                                <video class="w-full h-full object-fit rounded" src="https://v16-webapp.tiktok.com/f6db1968f055426059db7863652c7b28/61f02809/video/tos/useast2a/tos-useast2a-pve-0037-aiso/59c6542cf0aa4626af2a10e4609355d3/?a=1988&amp;br=1426&amp;bt=713&amp;cd=0%7C0%7C1%7C0&amp;ch=0&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;ft=XOQ9-3Adnz7Thq6g5lXq&amp;l=202201251040280102451411110607F173&amp;lr=tiktok&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=M2hvaWY6Zmh0OjMzZjgzM0ApNGc0Zzc2NTw5Nzc2aDg7OGc2ajQucjRvMWFgLS1kL2Nzc181LjAzNTMyYDQvMTFeYTU6Yw%3D%3D&amp;vl=&amp;vr=" playsinline controls ></video>
+                            </div>
+                            <div class="flex flex-col gap-4 justify-end">
+                                <div class="text-center">
+                                    <button class="rounded-full bg-gray-100 p-3 mb-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                    <span class="text-xs font-bold block">1.6M</span>
+                                </div>
+
+                                <div class="text-center">
+                                    <button class="rounded-full bg-gray-100 p-3 mb-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                    <span class="text-xs font-bold block">1.6M</span>
+                                </div>
+
+                                <div class="text-center">
+                                    <button class="rounded-full bg-gray-100 p-3 mb-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+                                        </svg>
+                                    </button>
+                                    <span class="text-xs font-bold block">1.6M</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="inline-flex flex-col gap-4 border-b pb-8">
+                        <div class="flex flex-row items-start gap-4">
+                            <img loading="lazy" src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/a50a0eff18403fdbf1d0b25bc03976e5.jpeg?x-expires=1643191200&amp;x-signature=NtwHCdDuRENkzcbnoqaCbNdjC0I%3D" class="h-16 w-16 rounded-full">
+                            <span class="flex-1 pt-2">
+                                <span class="font-black">
+                                    USERNAME
+                                    <span class="font-thin text-xs opacity-60">JOHN</span>
+                                </span>
+
+                                <p class="font-thin tracking-wide opacity-80">
+                                    Nulla adipisicing laborum deserunt est ut eiusmod cillum quis qui.
+                                </p>
+                            </span>
+
+                            <button class="border-primary text-primary border rounded px-5 font-semibold tracking-wider">Follow</button>
+                        </div>
+
+                        <div class="w-full flex flex-row pl-20 gap-4">
+                            <div class="rounded">
+                                <video class="w-full h-full object-fit rounded" src="https://v16-webapp.tiktok.com/fdc215b920672564d21147fa4ab12810/61f02809/video/tos/alisg/tos-alisg-pve-0037/e56dbe6f9b49426caf26ac85df6c3375/?a=1988&amp;br=2126&amp;bt=1063&amp;cd=0%7C0%7C1%7C0&amp;ch=0&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;ft=XOQ9-3Adnz7Thq6g5lXq&amp;l=202201251040280102451411110607F173&amp;lr=tiktok&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=ajllM2U6ZnE8OjMzODgzNEApODc6NzxpaTs8N2c0PGlnPGczLS0ucjRvc2dgLS1kLy1zczQ2NDUzLzA1LTNhY2NhYF46Yw%3D%3D&amp;vl=&amp;vr="  playsinline controls ></video>
+                            </div>
+                            <div class="flex flex-col gap-4 justify-end">
+                                <div class="text-center">
+                                    <button class="rounded-full bg-gray-100 p-3 mb-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                    <span class="text-xs font-bold block">1.6M</span>
+                                </div>
+
+                                <div class="text-center">
+                                    <button class="rounded-full bg-gray-100 p-3 mb-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                    <span class="text-xs font-bold block">1.6M</span>
+                                </div>
+
+                                <div class="text-center">
+                                    <button class="rounded-full bg-gray-100 p-3 mb-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+                                        </svg>
+                                    </button>
+                                    <span class="text-xs font-bold block">1.6M</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </perfect-scrollbar>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    beforeMount() {
+        this.initWidth();
+    },
+    mounted() {
+        window.addEventListener('resize', this.initWidth, true);
+    },
+    methods: {
+        initWidth() {
+            const container = document.getElementsByClassName('main');
+            if(!container.length) return;
 
+            const width = container[0].offsetWidth;
+            const header = document.getElementsByTagName('header');
+            if(!header.length) return;
+
+            document.getElementById('header-main').style.width = `${width}px`;
+        }
+    }
 }
 </script>
 

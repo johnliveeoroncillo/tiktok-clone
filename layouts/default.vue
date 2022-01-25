996a5526dc417b50fb0ae0790a266b49c829a4a1
John Livee Oroncillo <zjgoroncillo@917ventures.com>
2022-01-25 15:50:14 +0800

    Initial UI

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-screen h-screen overflow-hidden">
+        <div class="flex flex-col w-full h-full">
+            <template-header class="px-8 xl:px-40 2xl:px-96" />
+            <template-nav class="flex-1 xl:px-40 2xl:px-96" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
